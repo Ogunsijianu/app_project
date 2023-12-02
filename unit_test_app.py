76edb64dda57f1751157719752f256dba6873bd9
Ogunsijianu <135802471+Ogunsijianu@users.noreply.github.com>
2023-12-02 08:33:06 +0100

    Update unit_test_app.py

--- unit_test_app.py
+++ unit_test_app.py
@@ -1,21 +1,48 @@
-# unittest_app.py
 import unittest
-import app
+import calc
 
-class TestApp(unittest.TestCase):
+class TestCalc(unittest.TestCase):
+    """
+    Test the add2 function from the calc library
+    """
 
-    def test_add(self):
-        self.assertEqual(app.add(2, 3), 5)  # This test should pass
+    def test_add_integers(self):
+        """
+        Test that the addition of two integers returns the correct total
+        """
+        result = calc.add2(1, 2)
+        self.assertEqual(result, 3)
 
-    def test_subtract(self):
-        self.assertEqual(app.subtract(5, 3), 2)  # This test should pass
+    def test_add_floats(self):
+        """
+        Test that the addition of two floats returns the correct result
+        """
+        result = calc.add2('10.5', 2)
+        self.assertEqual(result, 12.5)
 
-    def test_multiply(self):
-        self.assertEqual(app.multiply(2, 3), 6)  # This test should pass
+    def test_add_strings(self):
+        """
+        Test the addition of two strings returns the two strings as one
+        concatenated string
+        """
+        result = calc.add2('abc', 'def')
+        self.assertEqual(result, 'abcdef')
 
-    def test_divide(self):
-        self.assertEqual(app.divide(6, 2), 3)  # This test should pass
-        self.assertEqual(app.divide(6, 0), 3)  # This test should fail
+    def test_add_string_and_integer(self):
+        """
+        Test the addition of a string and an integer returns them as one
+        concatenated string (in which the integer is converted to a string)
+        """
+        result = calc.add2('abc', 3)
+        self.assertEqual(result, 'abc3')
+
+    def test_add_string_and_float(self):
+        """
+        Test the addition of a string and a float returns them as one
+        concatenated string (in which the float is converted to a string)
+        """
+        result = calc.add2('abc', '5.5')
+        self.assertEqual(result, 'abc5.5')
 
 if __name__ == '__main__':
     unittest.main()
