85de6bb868b34b5028547a6b4d26d2b467436c02
Ogunsijianu <135802471+Ogunsijianu@users.noreply.github.com>
2023-12-02 08:30:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-# app.py
-def add(x, y):
-    return x + y
+import sys
+import calc
 
-def subtract(x, y):
-    return x - y
+def add_two_values(x, y):
+    try:
+        result = calc.add2(float(x), float(y))
+        return result
+    except ValueError:
+        return None
 
-def multiply(x, y):
-    return x * y
+def main():
+    if len(sys.argv) != 3:
+        print("\nIncorrect number of arguments. Usage: 'add2vals X Y'")
+        sys.exit(1)
 
-def divide(x, y):
-    if y == 0:
-        raise ValueError("Division by zero is not allowed")
-    return x / y
+    value1, value2 = sys.argv[1], sys.argv[2]
+
+    result = add_two_values(value1, value2)
+
+    if result is not None:
+        print(f"\nThe result of adding {value1} and {value2} is: {result}\n")
+        sys.exit(0)
+    else:
+        print("\nInvalid input. Please provide numeric values for addition.\n")
+        sys.exit(1)
